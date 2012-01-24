787ea16feeafc387d94340416024d0929941b09b
Samuele Arcidiacono <sarcidiacono@instart.co>
2012-01-24 12:52:33 +0100

    Multiple test-cases performed and graphed

--- js/parse-n-load.js
+++ js/parse-n-load.js
@@ -5,8 +5,18 @@
 //     console.log(files[0]);
 // }
 
+var SIMPLE = 0;
+var PARSE = 1;
+var PARSE_AS_STRING = 2;
+var PARSE_AND_EVALUATE = 3;
+var PARSE_AS_STRING_AND_EVALUATE = 4;
+var LABELS = [
+    'Simple', 'Parse only', 'Parse as string', 'Parse, then evaluate', 'eval() call'
+    ];
+var testcases = 5;
+
 function init() {
-    window.data = [];
+    window.data = [[],[],[],[],[]];
     window.jsframe = YAHOO.util.Dom.get('js');
     window.doc = YAHOO.util.Dom.get('js').contentWindow.document;
     window.win = YAHOO.util.Dom.get('js').contentWindow;
@@ -92,25 +102,31 @@ function nthPercentile(lst, n) {
 }
 
 function flotPlot(data) {
-    if (YAHOO.util.Dom.get('ignore-spikes').checked) {
-        data = nthPercentile(data, 0.95);
+    progress.innerHTML = (script.filename?
+            '<div><b>File:</b> '+script.filename+'</div>':
+            '')+
+        '<div><small>'+navigator.userAgent+'</small></div>';
+    
+    for(var testcase=0; testcase<testcases; testcase++) {
+        if (YAHOO.util.Dom.get('ignore-spikes').checked) {
+                data[testcase] = nthPercentile(data[testcase], 0.95);
+        }
+        var lst = map(function(x){return x[1];}, data[testcase]);
+        var mean = avg(lst);
+        var variance = stdev(lst, mean);
+        progress.innerHTML += [
+                              '<div><b>Testcase ',(testcase+1),': '+LABELS[testcase]+'</b></div>',
+                              '<div><b>Mean Average:</b> ',mean.toFixed(0),' msecs</div>',
+                              '<div><b>Std. Deviation:</b> ',variance.toFixed(1),' msecs</div>']
+                            .join('');
+        data[testcase] = {label: LABELS[testcase], data: data[testcase]};
     }
-    var lst = map(function(x){return x[1];}, data);
-    var mean = avg(lst);
-    var variance = stdev(lst, mean);
-    progress.innerHTML = [
-                          script.filename?'<div><b>File:</b> '+script.filename+'</div>':'',
-                          '<div><b>Mean Average:</b> ',mean.toFixed(0),' msecs</div>',
-                          '<div><b>Std. Deviation:</b> ',variance.toFixed(1),' msecs</div>',
-                          '<div><small>',navigator.userAgent,'</small></div>']
-                        .join('');
-    YAHOO.widget.Flot("flot", [data],
+    YAHOO.widget.Flot("flot", data,
                       {color:2,
                        lines:{show:true}
                       });
     YAHOO.util.Dom.get('browser-icon').src = 'img/icon-'+icon+'.png';
 }
-
 
 function time() {
     return (new Date()).getTime();
@@ -125,9 +141,10 @@ function delel(el) {
 }
 
 function recordTrial(msec) {
-    data[i] = [i, msec];
+    data[testcase][i] = [i, msec];
     if (i<runs) {
-        i += 1;
+        testcase = (testcase+1)%testcases;
+        if(testcase == 0) i += 1;
         loadFile2();
     } else {
         flotPlot(data);
@@ -143,21 +160,44 @@ function loadFile2() {
 }
 
 //for non-blocking browsers. careful not to blow the stack.
-function loadFile(i) {
+function loadFile(i, testcase) {
     if (i<runs) {
         doc.close();
         doc.write('<script>var start = (new Date()).getTime();</script>');
         doc.write('<script id="test" '+
         (script.filename?
             'src="'+script.filename+'">':
-            '>'+script.code)+
+            '>'+makeCodeFor(script.code, testcase))+
         '</script>');
-        doc.write('<script>top.data['+i+'] = ['+i+', (new Date()).getTime() - start];</script>');
+        doc.write('<script>top.data['+testcase+']['+i+'] = ['+i+', (new Date()).getTime() - start];</script>');
         doc.write('<script>var e=document.getElementById("test"); e.parentNode.removeChild(e);</script>');
-        doc.write('<script>window.setTimeout(function(){top.loadFile('+(i+1)+');}, 0);</script>');
+        testcase = (testcase+1)%testcases;
+        if(testcase==0) i++;
+        doc.write('<script>window.setTimeout(function(){top.loadFile('+i+', '+testcase+');});</script>');
         doc.close();
     } else {
         flotPlot(data);
+    }
+}
+
+function makeCodeFor(code, testcase) {
+    //XXX code string should be created before the timer starts
+    //TODO does it make any difference if the code is evaluated only once at the beginning of the test?
+    function escape(code) {
+        return code.replace(/\\/g, '\\\\').replace(/'/g, "\\'").replace(/\n/g, "'+\n'");
+    }
+    
+    switch(testcase) {
+        case SIMPLE:
+            return code;
+        case PARSE:
+            return 'function parse() { '+code+' }';
+        case PARSE_AS_STRING:
+            return 'function parse_as_string() { eval(\''+escape(code)+'\'); }';
+        case PARSE_AND_EVALUATE:
+            return 'function parse() { '+code+' } parse();';
+        case PARSE_AS_STRING_AND_EVALUATE:
+            return 'function parse_as_string() { eval(\''+escape(code)+'\'); } parse_as_string();';
     }
 }
 
@@ -169,7 +209,8 @@ var blocking = (match('Safari') && !match('Chrome') && match('Version/4')) || ma
 
 function runInit() {
     runs = parseInt(YAHOO.util.Dom.get('num-runs').value||'3');
-    data = new Array(runs);
+    data = new Array(testcases);
+    for(var i=0;i<testcases; i++) data[i] = new Array(runs);
     script.filename = YAHOO.util.Dom.get('js-file').value;
     script.code = YAHOO.util.Dom.get('js-code').value;
 }
@@ -188,21 +229,22 @@ function runTest() {
     // stack after just 20 iterations (ORLY? RLY.)
     // hence the window.setTimeout(fn, 0);
     if (blocking) {
-        for (var i=0; i<runs; i++) {
-            doc.open();
-            var start = time();
-            doc.write('<script id="test" '+
-            (script.filename?
-                'src="'+script.filename+'">':
-                '>'+script.code)+
-            '</script>');
-            doc.write('<script>var e=document.getElementById("test"); e.parentNode.removeChild(e);</script>');
-            doc.close();
-            data[i] = [i, (new Date()).getTime() - start];
-        }
+        for (var i=0; i<runs; i++) 
+            for (var testcase=0; testcase<testcases; testcase++) {
+                doc.open();
+                var start = time();
+                doc.write('<script id="test" '+
+                (script.filename?
+                    'src="'+script.filename+'">':
+                    '>'+makeCodeFor(script.code, testcase))+
+                '</script>');
+                doc.write('<script>var e=document.getElementById("test"); e.parentNode.removeChild(e);</script>');
+                doc.close();
+                data[testcase][i] = [i, (new Date()).getTime() - start];
+            }
         flotPlot(data);
     } else {
-        window.setTimeout(function(){loadFile(0);}, 0);
+        window.setTimeout(function(){loadFile(0,0);}, 0);
     }
 }
 
